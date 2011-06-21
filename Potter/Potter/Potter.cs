5cf0a5db2e9b9cb0e4b2f9e7f8e783d273876d55
unknown <karen.zamlutti@.apontador.corp.br>
2011-06-21 19:09:05 -0300

    Dojo 21/06/2011

---
+++ Potter/Potter/Potter.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+namespace Potter
+{
+    public class Potter
+    {
+        private readonly IList<BookTitle> _listOfBooks;
+        private readonly int _books;
+        private const double InitialPrice = 20.0;
+        private static readonly Dictionary<int, double> Discounts = new Dictionary<int, double>
+                                                        {
+                                                            {1,1},
+                                                            {2,0.95},
+                                                            {3,0.90},
+                                                            {4,0.80},
+                                                            {5,0.75}
+                                                        }; 
+
+        public Potter(int books)
+        {
+            _books = books;
+        }
+
+        public Potter(IList<BookTitle> listOfBooks)
+        {
+            _listOfBooks = listOfBooks;
+        }
+
+        public double Total()
+        {
+            List<BookTitle> listOfUniqueBooks = new List<BookTitle>();
+            foreach (var book in _listOfBooks)
+            {
+                if (!listOfUniqueBooks.Contains(book))
+                {
+                    listOfUniqueBooks.Add(book);
+                }
+            }
+            return InitialPrice * _books * Discounts[_books];
+        }
+    }
+
+    public enum BookTitle
+    {
+        First,
+        Second,
+        Third,
+        Fourth,
+        Fifth
+    }
+}
---
+++ Potter/UnitTest/PotterTest.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Potter;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class PotterTest
+    {
+        [TestMethod]
+        public void TestFullValueWithOneBook ()
+        {
+            Potter.Potter potter = new Potter.Potter(1);
+            Assert.AreEqual(20, potter.Total());
+        }
+
+
+        [TestMethod]
+        public void Test5PercentOffWhenTwoBooks()
+        {
+            Potter.Potter potter = new Potter.Potter(2);
+            Assert.AreEqual(38, potter.Total());
+        }
+
+        [TestMethod]
+        public void Test10PercentOffWhenThreeBooks()
+        {
+            Potter.Potter potter = new Potter.Potter(3);
+            Assert.AreEqual(54, potter.Total());
+        }
+
+        [TestMethod]
+        public void Test20PercentOffWhenFourBooks()
+        {
+            Potter.Potter potter = new Potter.Potter(4);
+            Assert.AreEqual(64, potter.Total());
+        }
+
+        [TestMethod]
+        public void Test25PercentOffWhenFourBooks()
+        {
+            Potter.Potter potter = new Potter.Potter(5);
+            Assert.AreEqual(75, potter.Total());
+        }
+
+        [TestMethod]
+        public void TestFullPriceGivenTwoEqualBooks()
+        {
+            var listOfBooks = new List<BookTitle>();
+            listOfBooks.Add(BookTitle.Fifth);
+            listOfBooks.Add(BookTitle.Fifth);
+            Potter.Potter potter = new Potter.Potter(listOfBooks);
+            Assert.AreEqual(40, potter.Total());
+        }
+    }
+}
